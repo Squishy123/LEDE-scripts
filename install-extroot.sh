8fda5b432ac1a914f4eb45dd0c4a66dfd1d6de58
Christian Wang <christian.gnaw@gmail.com>
2017-09-05 12:43:25 -0400

    Added project files

---
+++ install-extroot.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+clear
+
+echo "WELCOME TO EXTROOT CONFIG"
+
+while true; do
+
+  read -n1 -r -p "$(echo -e 'Install Package Dependencies? (IF YOU HAVE MORE THAN 8MB OF FLASH) Y/N\n\b')" yn
+  case $yn in
+    [Yy]* ) echo "Installing Packages...";
+    opkg update && opkg install block-mount  kmod-fs-f2fs kmod-usb-storage mkf2fs f2fsck kmod-usb-ohci kmod-usb-uhci fdisk
+    break;;
+    [Nn]* ) break;;
+    *) echo "Please answer yes or no"
+  esac
+done
+
+
+echo "Please select the mounted USB (TODO CUSTOM QUERY -> Currently grabbing usb device at /dev/sda1)"
+block info
+
+#Formatting the usb to f2fs
+echo "Formatting the USB to f2fs"
+mkfs.f2fs /dev/sda1
+
+echo "Transferring the current overlay into the USB"
+mount /dev/sda1 /mnt ; tar -C /overlay -cvf - . | tar -C /mnt -xf - ; umount /mnt
+
+echo "Creating fstab uci subsystem and configuring it to use /dev/sda1 as it's overlay"
+block detect > /etc/config/fstab; \
+   sed -i s/option$'\t'enabled$'\t'\'0\'/option$'\t'enabled$'\t'\'1\'/ /etc/config/fstab; \
+   sed -i s#/mnt/sda1#/overlay# /etc/config/fstab; \
+   cat /etc/config/fstab;
+
+echo "Mounting /dev/sda1 as /overlay"
+mount /dev/sda1 /overlay
+
+echo "Check to make sure /dev/sda1 is mounted as /overlay"
+df
+
+echo "EXTROOT INSTALL COMPLETE"
+echo "REBOOTING..."
+reboot
---
+++ install-stock-firmware.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Script for Soft-Bricking WR902-AC
+
+clear
+echo "DOWNLOADING FIRMWARE IMAGE"
+wget 'http://static.tp-link.com/TL-WR902AC(US)_V1_160905.zip'
+
+echo "EXTRACTING IMAGE"
+unzip 'TL-WR902AC(US)_V1_160905.zip'
+
+echo "RENAMING"
+mv "TL-WR902AC(US)_V1_160905/"*.bin original.bin
+
+while true; do
+  clear 
+  read -n1 -r -p "$(echo -e 'CONNECT TO THE ROUTER NOW. WHEN READY TYPE "Y" TO START FIRMWARE INSTALL')" y
+  case $y in
+    [Yy]* )
+    echo "CONNECTING TO ROUTER"
+    echo "COPYING OVER FIRMWARE FILE"
+    scp 'original.bin' root@192.168.1.1:/tmp
+    ssh root@192.168.1.1 'sysupgrade -F /tmp/original.bin'
+    exit
+    break;;
+    [Nn]* ) break;;
+    *) echo "PLEASE ANSWER YES OR NO"
+  esac
+done
+
+echo "EXITING.."
+~
+~
+~
+~
